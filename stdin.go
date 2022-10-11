73fd1293432ad3243185b22ea06d14066c428d3f
VazhikVM <v.vazhinskiy@yandex.ru>
2022-10-11 10:21:48 +0300

    new

---
+++ stdin.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+import "math"
+
+
+func main() {
+	my_test()
+}
+
+func my_test() {
+	var num float64;
+	var ex string;
+
+	for {
+		fmt.Println("Введите десятичное число: ")
+		fmt.Scan(&num)
+
+		if (num <= 0){
+			fmt.Printf("Число R не подходи%2.2f\n", num)
+		} else if (num >10000) {
+			fmt.Printf("Число R не подходит %e\n", num)
+		} else {
+			fmt.Printf("%.4f\n", math.Pow(num, 2))
+		}
+
+		fmt.Println("Для выхода введете 'exit' и нажмите enter: ")
+		fmt.Scan(&ex)
+
+		if (ex == "exit") {
+			break
+		}
+	}
+}
